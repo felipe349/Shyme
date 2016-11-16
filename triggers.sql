11198a81c5dd9d3121a0fa747811c7a1840d5356
Guganr <gustavonori95@gmail.com>
2016-11-16 05:15:16 -0200

    Add files via upload

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,5 @@
+
+DELIMITER $$
 CREATE TRIGGER sairGrupo AFTER UPDATE ON ALUNO_GRUPO
 FOR EACH ROW
 BEGIN
@@ -27,3 +29,86 @@ BEGIN
     end if;
 
 END $$
+
+
+DELIMITER $$
+
+CREATE  PROCEDURE `adicionarMembro`(in aluno int(11), in grupo int(10))
+begin
+declare cd_aluno int(11);
+declare cd_grupo int(10);
+SELECT cd_aluno = count(*) from aluno_grupo
+where cd_matricula = aluno and cd_grupo = grupo;
+
+if (cd_aluno > 0)
+then
+	update aluno_grupo set ic_aluno_Grupo = 1 where cd_matricula = aluno and cd_grupo = grupo;
+else
+	INSERT INTO aluno_grupo (cd_grupo, cd_matricula,prioridade_grupo,IC_ALUNO_GRUPO) VALUES (grupo,aluno,0,1);
+end if;
+end $$
+
+DELIMITER $$
+
+CREATE PROCEDURE `atribuirMoeda`(IN vlMoeda INT, IN icPost int, IN cdAluno INT)
+BEGIN 
+declare moedanew,moedant, ponto int;
+set moedant = (SELECT VL_MOEDA FROM ALUNO WHERE CD_MATRICULA =  cdAluno);
+	UPDATE aluno SET vl_moeda = CASE 
+	WHEN icPost = 0 THEN vl_moeda-vlMoeda 
+	WHEN icPost = 1 THEN vl_moeda+vlMoeda 
+	ELSE vl_moeda 
+	END 
+    WHERE cd_matricula = cdAluno;
+set moedanew = (SELECT VL_MOEDA FROM ALUNO WHERE CD_MATRICULA =  cdAluno); 
+   
+	IF moedanew > moedant
+    then
+    set ponto = moedanew - moedant;
+    call atribuirPontuacao(ponto,cdAluno);
+    END IF;
+END $$
+
+DELIMITER $$
+
+CREATE PROCEDURE `atribuirPontuacao`(IN ponto int, IN cdAluno INT)
+BEGIN 
+INSERT INTO PONTO_ALUNO VALUES(cdAluno,ponto,now());
+END $$
+
+DELIMITER $$
+
+CREATE  PROCEDURE `trocarGrupoPrincipal`(IN aluno INT, IN grupoPrimario INT, IN grupoSecundario INT)
+BEGIN
+	UPDATE ALUNO_GRUPO SET prioridade_grupo = CASE
+		WHEN CD_MATRICULA = aluno AND CD_GRUPO = grupoPrimario AND PRIORIDADE_GRUPO = 0 THEN 1
+		WHEN CD_MATRICULA = aluno AND CD_GRUPO = grupoPrimario AND PRIORIDADE_GRUPO = 1 THEN 0
+		WHEN CD_MATRICULA = aluno AND CD_GRUPO = grupoSecundario AND PRIORIDADE_GRUPO = 0 THEN 1
+		WHEN CD_MATRICULA = aluno AND CD_GRUPO = grupoSecundario AND PRIORIDADE_GRUPO = 1 THEN 0
+        ELSE PRIORIDADE_GRUPO
+        END;
+END $$
+
+DELIMITER $$
+
+CREATE  PROCEDURE `verificarMoeda`(IN tp INT,  IN cdAluno INT)
+BEGIN
+declare moeda,tpmoeda,icpost int;
+
+set tpmoeda = (SELECT T.VL_TIPO_POSTAGEM FROM ALUNO A, TIPO_POSTAGEM T
+WHERE T.CD_TIPO_POSTAGEM = tp AND A.CD_MATRICULA = cdAluno);
+
+set moeda = (SELECT A.VL_MOEDA FROM ALUNO A, TIPO_POSTAGEM T
+WHERE T.CD_TIPO_POSTAGEM = tp AND A.CD_MATRICULA = cdAluno);
+
+set icpost = (SELECT T.IC_TIPO_POSTAGEM FROM ALUNO A, TIPO_POSTAGEM T
+WHERE T.CD_TIPO_POSTAGEM = tp AND A.CD_MATRICULA = cdAluno);
+
+
+ IF(moeda >= tpmoeda)
+THEN
+ CALL atribuirMoeda(tpmoeda,icpost,cdAluno);
+
+END IF; 
+END $$
+
